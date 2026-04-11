1a7900972b86d2b094245c34491344ee4e82e65c
Ishadh Ifham <ishadhifham@gmail.com>
2026-04-11 08:54:39 +0530

    backend setup

---
+++ submanager/submanager/src/main/java/com/ishadh/submanager/SubmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.ishadh.submanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmanagerApplication.class, args);
+	}
+
+}
